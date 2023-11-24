31ee182a69d0d3389e77ca96d2d26f10b2758b9b
lursz <lukoszr@gmail.com>
2023-11-24 12:57:17 +0100

    Lab6 end of lesson

--- lab6/ex3/js/script.js
+++ lab6/ex3/js/script.js
@@ -1,68 +1,63 @@
-async function getCitiesData() {
-    let newObject = await fetch('./city.json');
-    return await newObject.json();
-}
-
-function getA(cities) {
-    let answer = "";
+function qA(cities) {
+    let result = "";
     cities.forEach(element => {
-        (element.province == "małopolskie") ? answer += element.name + ", " : null;
+        (element.province == "małopolskie") ? result += `${element.name}, ` : null;
     });
-    answer = answer.substring(0, answer.length - 2) + ".";
-    document.getElementById("question-a").innerHTML = answer;
+    result = result.substring(0, result.length - 2) + ".";
+    document.getElementById("question-a").innerHTML = result;
 }
 
-function getB(cities) {
-    let answer = "";
-    const regExp = /^[^aA]*[aA][^aA]*[aA][^aA]*$/
+function qB(cities) {
+    let result = "";
+    const regex = /^[^aA]*[aA][^aA]*[aA][^aA]*$/
     cities.forEach(element => {
-        (regExp.test(element.name)) ? answer += element.name + ", " : null;
+        (regex.test(element.name)) ? result += element.name + ", " : null;
     });
-    answer = answer.substring(0, answer.length - 2) + ".";
-    document.getElementById("question-b").innerHTML = answer;
+    result = result.substring(0, result.length - 2) + ".";
+    document.getElementById("question-b").innerHTML = result;
 }
 
-function getC(cities) {
-    let answer = "";
-    const sortedCities = [...cities].sort(function (a, b) { return b.dentensity - a.dentensity });
-    answer = sortedCities[4].name + ".";
-    document.getElementById("question-c").innerHTML = answer;
+function qC(cities) {
+    let result = "";
+    // ... - spread operator to copy array
+    let sortedCities = [...cities].sort((a, b) => { return b.density - a.density });
+    result = sortedCities[4].name + ".";
+    document.getElementById("question-c").innerHTML = result;
 }
 
-function getD(cities) {
-    let answer = "";
+function qD(cities) {
+    let result = "";
     cities.forEach(element => {
-        if (element.people > 100000) answer += element.name + " City, "
+        if (element.people > 100000) result += `${element.name} City, `;
     })
-    document.getElementById("question-d").innerHTML = answer;
+    document.getElementById("question-d").innerHTML = result;
 }
 
-function getE(cities) {
+function qE(cities) {
     let higher = 0;
     let lower = 0;
     cities.forEach(element => {
         (element.people > 80000) ? higher += 1 : lower += 1;
     })
-    let answer = "Więcej jest miast " + ((higher > lower) ? "powyżej " : "poniżej ") + "80 000 mieszkańców. " +
-        "Miast powyżej 80 000 jest " + higher + ", a poniżej " + lower + ".";
-    document.getElementById("question-e").innerHTML = answer;
+    let result = `Więcej jest miast ${higher > lower ? "powyżej" : "poniżej"} 80 000 mieszkańców. Miast powyżej 80 000 jest ${higher}, poniżej ${lower}.`
+    document.getElementById("question-e").innerHTML = result;
 }
 
-function getF(cities) {
+function qF(cities) {
     let sum = 0;
     let count = 0;
     cities.forEach(element => {
-        if (element.township.startsWith("p")) {
+        if (element.township.startsWith("P")) {
             sum += element.area;
             count += 1;
         };
     })
     let average = sum / count;
-    let answer = average.toPrecision(5) + ", z dokładnością do 5 cyfr znaczących."
-    document.getElementById("question-f").innerHTML = answer;
+    let result = average.toPrecision(5);
+    document.getElementById("question-f").innerHTML = result;
 }
 
-function getG(cities) {
+function qG(cities) {
     let count = 0;
     let countHigher = 0;
     cities.forEach(element => {
@@ -71,23 +66,29 @@ function getG(cities) {
             if (element.people > 5000) countHigher += 1;
         };
     })
-    let answer = ((count == countHigher) ? "Wszystkie " : "Nie wszystkie ") + "miasta w województwie pomorskim mają ponad 5 000 mieszkańców. " + "<br>" +
-        "Ilość miast posiadających więcej niż 5tyś mieszkańców: " + countHigher + "." + "<br>" +
-        "Ilość wszystkich miast w tym województwie: " + count + ".";
-    document.getElementById("question-g").innerHTML = answer;
+
+    let result = `${(count == countHigher) ? "Wszystkie " : "Nie wszystkie "}miasta w województwie pomorskim mają więcej niż 5 000 mieszkańców. <br>
+    Ilość miast z więcej niż 5 000 mieszkańców: ${countHigher}. <br>
+    Ilość wszystkich miast w województwie: ${count}.`;
+
+    document.getElementById("question-g").innerHTML = result;
 }
 
+async function getCitiesData() {
+    let newObject = await fetch('./city.json');
+    return await newObject.json();
+}
 
-async function run() {
+async function init() {
     const Cities = await getCitiesData();
-    getA(Cities);
-    getB(Cities);
-    getC(Cities);
-    getD(Cities);
-    getE(Cities);
-    getF(Cities);
-    getG(Cities);
+    qA(Cities);
+    qB(Cities);
+    qC(Cities);
+    qD(Cities);
+    qE(Cities);
+    qF(Cities);
+    qG(Cities);
 }
 
-run();
+init();
 
